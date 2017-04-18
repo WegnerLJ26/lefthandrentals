b66d447a69dad7a7f4479ad266548e76d7ea880b
Logan Wegner <WegnerLJ26@uww.edu>
2017-04-17 22:41:52 -0500

    New Sidebar Created

--- app/src/main/java/righthandrentals/righthandrentals/SideBar.java
+++ app/src/main/java/righthandrentals/righthandrentals/SideBar.java
@@ -13,13 +13,13 @@ import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 
-public class SideMenu extends AppCompatActivity
+public class SideBar extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_side_menu);
+        setContentView(R.layout.activity_side_bar);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
@@ -55,7 +55,7 @@ public class SideMenu extends AppCompatActivity
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.side_menu, menu);
+        getMenuInflater().inflate(R.menu.side_bar, menu);
         return true;
     }
 
@@ -87,6 +87,10 @@ public class SideMenu extends AppCompatActivity
         } else if (id == R.id.nav_slideshow) {
 
         } else if (id == R.id.nav_manage) {
+
+        } else if (id == R.id.nav_share) {
+
+        } else if (id == R.id.nav_send) {
 
         }
 
