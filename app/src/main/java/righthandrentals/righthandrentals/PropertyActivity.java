69454f97f5dab84b67a8f6b6657f5986ad6dd265
Logan Wegner <WegnerLJ26@uww.edu>
2017-05-02 08:11:10 -0500

    Updated Listeners

--- app/src/main/java/righthandrentals/righthandrentals/PropertyActivity.java
+++ app/src/main/java/righthandrentals/righthandrentals/PropertyActivity.java
@@ -1,13 +1,23 @@
 package righthandrentals.righthandrentals;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.ImageButton;
 
 public class PropertyActivity extends AppCompatActivity {
+
+    ImageButton i1;
+    ImageButton i2;
+    ImageButton i3;
+    ImageButton i4;
+    ImageButton i5;
+    ImageButton i6;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,9 +32,66 @@ public class PropertyActivity extends AppCompatActivity {
             public void onClick(View view) {
                 Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
                         .setAction("Action", null).show();
+
+                addListenerOnButton();
             }
         });
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+    }
+
+    public void addListenerOnButton() {
+
+        i1 = (ImageButton) findViewById(R.id.imageButton2);
+        i2 = (ImageButton) findViewById(R.id.imageButton);
+        i3 = (ImageButton) findViewById(R.id.imageButton4);
+        i4 = (ImageButton) findViewById(R.id.imageButton5);
+        i5 = (ImageButton) findViewById(R.id.imageButton6);
+        i6 = (ImageButton) findViewById(R.id.imageButton7);
+
+        i1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(PropertyActivity.this, PropertyProfileActivity.class));
+            }
+        });
+
+        i2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(PropertyActivity.this, PropertyProfileActivity.class));
+            }
+        });
+
+        i3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(PropertyActivity.this, PropertyProfileActivity.class));
+            }
+        });
+
+        i4.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(PropertyActivity.this, PropertyProfileActivity.class));
+            }
+        });
+
+        i5.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(PropertyActivity.this, PropertyProfileActivity.class));
+            }
+        });
+
+        i6.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(PropertyActivity.this, PropertyProfileActivity.class));
+            }
+        });
+
+
+
     }
 
 }
